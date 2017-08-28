58daa5415170ac6ab62600d144f65ace62b3bce2
Gursimran Singh <gursimransingh.gsh@gmail.com>
2017-08-28 13:43:24 +0530

    Djangoapp tutorial part 04 commit 1.

--- tutorialapp/polls/views.py
+++ tutorialapp/polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -31,8 +33,35 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-	return HttpResponse("You are looking at results of question %s." % question_id)
+	question = get_object_or_404(Question, pk = question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-	return HttpResponse("You are voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk = question_id)
+
+	try:
+		selected_choice = Choice.objects.get(pk = request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		# There is a possibility of a race condition while updating values in a db.
+		# selected_choice.votes += 1
+		# selected_choice.save()
+
+		# Use F() to get around race conditions
+		selected_choice.votes = F('votes') + 1
+		selected_choice.save()
+
+		# NOTE: F() objects persist after save()
+		# To stop it from running on every save just refresh the object from db again
+		selected_choice.refresh_from_db()
+
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
